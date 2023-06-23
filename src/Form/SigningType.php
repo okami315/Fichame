16b8373d32d487773ae34b14811c5a766250d532
jose_016al <jose_016al@outlook.com>
2023-06-23 16:49:54 +0200

    Modifiacion de entidad registration a signing

---
+++ src/Form/SigningType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Signing;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SigningType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('checkin')
+            ->add('checkout')
+            ->add('createcheckin')
+            ->add('createcheckout')
+            ->add('event')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Signing::class,
+        ]);
+    }
+}
